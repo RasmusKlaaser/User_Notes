75e78976bcef9d97c367df79093ece877dd35f71
Rasmus <rasmus.klaaser@voco.ee>
2024-03-04 09:32:59 +0200

    Fix existing user and password limitation errors

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,22 @@ const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
 const register = (req, res) => {
+const register = async (req, res) => {
     console.log(req.body)
+    const usernames = await User.findAll({ where: { username: req.body.username } })
+    if (usernames.length > 0) {
+        return res.status(500).json({ message: 'Username already exists!' })
+    }
+
+    const emails = await User.findAll({ where: { email: req.body.email } })
+    if (emails.length > 0) {
+        return res.status(500).json({ message: 'Email already registered!' })
+    }
+
+    if (req.body.password.length < 8) {
+        return res.status(500).json({ message: 'Password must be 8 characters or longer!'})
+    }   
+
     bcrypt.hash(req.body.password, 10, (error, cryptPassword) => {
         User.create({
             username: req.body.username,
@@ -12,16 +27,17 @@ const register = (req, res) => {
         .then((registered) => {
             req.session.user = {
                 username: registered.username,
-                user_id: registered_id,
+                user_id: registered.id
             };
             console.log(req.session)
             res.json({
-                message: 'New user is registered',
+                message: 'New user is registered', 
                 user: registered,
                 user_session: req.session.user
-            }) 
+            })
         })
     })
 } 
+}   
 
 module.exports = {register} 
